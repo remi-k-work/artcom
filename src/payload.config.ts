439d21c3d72b3981c77d05de3f2839f89bce0190
Remi <remi.k.work@gmail.com>
2024-11-26 20:24:05 +0100

    adding pl lang to the admin panel

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,9 +1,10 @@
-// storage-adapter-import-placeholder
 import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import { vercelPostgresAdapter } from "@payloadcms/db-vercel-postgres";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
+import { en } from "@payloadcms/translations/languages/en";
+import { pl } from "@payloadcms/translations/languages/pl";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
 
@@ -29,4 +30,5 @@ export default buildConfig({
   db: vercelPostgresAdapter({ pool: { connectionString: process.env.POSTGRES_URL || "" } }),
   sharp,
   plugins: [vercelBlobStorage({ collections: { media: true }, token: process.env.BLOB_READ_WRITE_TOKEN || "" })],
+  i18n: { supportedLanguages: { en, pl } },
 });
