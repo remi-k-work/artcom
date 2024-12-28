0004218a064354d0beadab479a889eb9ec3cb6e3
Remi <remi.k.work@gmail.com>
2024-12-28 12:40:27 +0100

    access for the users collection

--- src/access/isAdminOrSelf.ts
+++ src/access/isAdminOrSelf.ts
@@ -2,30 +2,17 @@
 import type { Access } from "payload";
 import type { User } from "@/payload-types";
 
+// access control functions
 import { isAdmin } from "./isAdmin";
 
+// If the user has the role of "admin" or any other type of user
 export const isAdminOrSelf: Access<User> = (args) => {
-  // Needs to be logged in
-  const {req: {user}} = args;
-  if( !user ) return false;
+  // Needs to be logged in first
+  const {
+    req: { user },
+  } = args;
+  if (!user) return false;
 
-  // If the user has the role of "admin" or any other type of user
-  isAdmin(args) || {id: {equals: user.id}}
-  if( )
-  if (user) {
-    // If user has role of 'admin'
-    if (user.roles?.includes('admin')) {
-      return true;
-    }
-
-    // If any other type of user, only provide access to themselves
-    return {
-      id: {
-        equals: user.id,
-      }
-    }
-  }
-
-  // Reject everyone else
-  return false;
-}
+  // If any other type of user, only provide access to themselves
+  return isAdmin(args) || { id: { equals: user.id } };
+};
--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,6 +1,12 @@
+// payload and db access
 import type { CollectionConfig } from "payload";
 
+// fields
 import text from "./fields/text";
+
+// access control functions
+import { isAdmin, isAdminFieldLevel } from "@/access/isAdmin";
+import { isAdminOrSelf } from "@/access/isAdminOrSelf";
 
 export const Users: CollectionConfig<"users"> = {
   slug: "users",
@@ -8,11 +14,26 @@ export const Users: CollectionConfig<"users"> = {
   labels: { singular: { en: "User", pl: "Użytkownik" }, plural: { en: "Users", pl: "Użytkownicy" } },
   admin: { defaultColumns: ["username", "name", "email", "role"], useAsTitle: "username", hideAPIURL: true },
 
+  access: {
+    // Only admins can create users
+    create: isAdmin,
+
+    // Admins can read all, but any other logged-in user can only read themselves
+    read: isAdminOrSelf,
+
+    // Admins can update all, but any other logged-in user can only update themselves
+    update: isAdminOrSelf,
+
+    // Only admins can delete users
+    delete: isAdmin,
+  },
+
   auth: {
-    loginWithUsername: {
-      allowEmailLogin: true,
-      requireEmail: true,
-    },
+    loginWithUsername: { allowEmailLogin: true, requireEmail: true },
+
+    // This property controls how deeply "populated" relationship docs are that are stored in the req.user
+    // It should be kept as low as possible, which keeps performance fast
+    depth: 0,
   },
 
   timestamps: true,
@@ -42,6 +63,12 @@ export const Users: CollectionConfig<"users"> = {
       ],
 
       defaultValue: "user",
+
+      access: {
+        // Only admins can create or update a value for this field
+        create: isAdminFieldLevel,
+        update: isAdminFieldLevel,
+      },
     },
   ],
 };
