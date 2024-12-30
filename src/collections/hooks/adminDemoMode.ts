e3be950261335d805b1a3b9f7242c75efec0840d
Artcom <artcom@gmail.com>
2024-12-30 12:55:50 +0100

    demo mode error showing in an admin's toast

--- src/collections/hooks/adminDemoMode.ts
+++ src/collections/hooks/adminDemoMode.ts
@@ -1,12 +1,23 @@
 // payload and db access
-import type { CollectionBeforeChangeHook, CollectionBeforeDeleteHook } from "payload";
+import { APIError, type CollectionBeforeChangeHook, type CollectionBeforeDeleteHook } from "payload";
+import type { DefaultTranslationKeys, TFunction } from "@payloadcms/translations";
+
+import { CustomTranslationsKeys } from "@/custom-translations";
+
+class DemoModeError extends APIError {
+  constructor(message: string) {
+    super(message, 400, undefined, true);
+  }
+}
 
 // Prevent any modifications to the documents in the admin panel from being saved if the user is in demo mode
-export const adminDemoModeBeforeChange: CollectionBeforeChangeHook = async ({ data, req: { user } }) => {
-  if (user?.username === "demo") throw new Error("Changes cannot be saved in demo mode.");
+export const adminDemoModeBeforeChange: CollectionBeforeChangeHook = async ({ data, req: { i18n, user } }) => {
+  const t = i18n.t as TFunction<CustomTranslationsKeys | DefaultTranslationKeys>;
+  if (user?.username === "demo") throw new DemoModeError(t("custom:demoModeErrChange"));
   return data;
 };
 
-export const adminDemoModeBeforeDelete: CollectionBeforeDeleteHook = async ({ req: { user } }) => {
-  if (user?.username === "demo") throw new Error("You cannot delete data in demo mode.");
+export const adminDemoModeBeforeDelete: CollectionBeforeDeleteHook = async ({ req: { i18n, user } }) => {
+  const t = i18n.t as TFunction<CustomTranslationsKeys | DefaultTranslationKeys>;
+  if (user?.username === "demo") throw new DemoModeError(t("custom:demoModeErrDelete"));
 };
--- src/custom-translations.ts
+++ src/custom-translations.ts
@@ -5,16 +5,20 @@ export const customTranslations: Config["i18n"] = {
     en: {
       custom: {
         valInvalidUrl: "Please enter a valid URL",
+        demoModeErrChange: "Changes cannot be saved in demo mode!",
+        demoModeErrDelete: "You cannot delete data in demo mode!",
       },
     },
 
     pl: {
       custom: {
         valInvalidUrl: "Proszę podać właściwy adres URL",
+        demoModeErrChange: "Zmian nie można zapisać w trybie demonstracyjnym!",
+        demoModeErrDelete: "Nie można usuwać danych w trybie demonstracyjnym!",
       },
     },
   },
 };
 
 export type CustomTranslationsObject = typeof customTranslations.translations;
-export type CustomTranslationsKeys = "custom:valInvalidUrl";
+export type CustomTranslationsKeys = "custom:valInvalidUrl" | "custom:demoModeErrChange" | "custom:demoModeErrDelete";
